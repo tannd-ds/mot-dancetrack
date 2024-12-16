41c0c7f860459e93db89db8bdf6b2d972e06c013
dannto <donhattan1012dev@gmail.com>
2024-12-16 16:57:42 +0700

    improve generate() function

--- models/simple.py
+++ models/simple.py
@@ -13,16 +13,16 @@ class BaseModel(nn.Module):
     def generate(self, conditions, img_w, img_h, **kwargs):
         cond_encodeds = []
         for i in range(len(conditions)):
-            tmp_c = conditions[i]
-            tmp_c = np.array(tmp_c)
-            tmp_c[:, 0::2] = tmp_c[:, 0::2] / img_w
-            tmp_c[:, 1::2] = tmp_c[:, 1::2] / img_h
-            tmp_conds = torch.tensor(tmp_c, dtype=torch.float)
-            if len(tmp_conds) != self.config['interval']:
-                pad_conds = tmp_conds[-1].repeat((self.config['interval'], 1))
-                tmp_conds = torch.cat((tmp_conds, pad_conds), dim=0)[:self.config['interval']]
-            cond_encodeds.append(tmp_conds.unsqueeze(0))
+            tmp_c = torch.tensor(np.array(conditions[i]), dtype=torch.float)
+            # pad the condition to the interval
+            if len(tmp_c) != self.config['interval']:
+                pad_conds = tmp_c[-1].repeat((self.config['interval'] - len(tmp_c), 1))
+                tmp_c = torch.cat((tmp_c, pad_conds), dim=0)
+            cond_encodeds.append(tmp_c.unsqueeze(0))
+
         cond_encodeds = torch.cat(cond_encodeds)
+        cond_encodeds[:, :, 0::2] = cond_encodeds[:, :, 0::2] / img_w
+        cond_encodeds[:, :, 1::2] = cond_encodeds[:, :, 1::2] / img_h
         track_pred = self.forward(cond_encodeds.to("cuda"))
         return track_pred.cpu().detach().numpy()
 
