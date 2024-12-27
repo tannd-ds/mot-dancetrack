cccb81b4741e95b3667a068debeedf513859c22c
21522173huy <tulehuy.003@gmail.com>
2024-12-27 17:33:32 +0700

    first add

--- models/Convolution.py
+++ models/Convolution.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from collections import OrderedDict
 from models.Base import BasePositionPredictor
 
 
@@ -123,7 +123,7 @@ class ResNetPredictor(BasePositionPredictor):
 
 
 class Conv2dPredictor(BasePositionPredictor):
-    def __init__(self, config, input_dim=8):
+    def __init__(self, config, input_dim=8, output_dim=4):
         super(Conv2dPredictor, self).__init__(config)
         self.input_dim = input_dim
         self.conv1 = nn.Conv2d(in_channels=1,
@@ -153,7 +153,7 @@ class Conv2dPredictor(BasePositionPredictor):
                                padding=0)
 
         self.fc1 = nn.Linear(self.input_dim * (self.config['interval']-4) * 128, 64)
-        self.fc2 = nn.Linear(64, 4)
+        self.fc2 = nn.Linear(64, output_dim)
 
     def forward(self, conditions):
         x = conditions.view(-1, 1, self.input_dim, self.config['interval']) # (batch_size, 1, 8, interval)
@@ -163,6 +163,70 @@ class Conv2dPredictor(BasePositionPredictor):
         x = torch.relu(self.conv4(x)) # (batch_size, 256, 8, interval-2)
         x = torch.relu(self.conv5(x)) # (batch_size, 128, 8, interval-4)
         x = x.view(x.size(0), -1) # (batch_size, 8 * (interval-4) * 128)
-        x = torch.relu(self.fc1(x))
-        delta_bbox = self.fc2(x)
+        x = torch.relu(self.fc1(x)) # (batch_size, 64)
+        delta_bbox = self.fc2(x) # (batch_size, output_dim)
         return delta_bbox
+    
+class Huy_Conv2dPredictor(BasePositionPredictor):
+    def __init__(self, config, input_dim=8):
+        super(Huy_Conv2dPredictor, self).__init__(config)
+        self.input_dim = input_dim
+        self.config = config
+        self.conv1 = nn.Conv2d(in_channels=1,
+                               out_channels=32,
+                               kernel_size=(1, 3),
+                               stride=1,
+                               padding=(0, 1))
+        self.conv2 = nn.Conv2d(in_channels=32,
+                               out_channels=64,
+                               kernel_size=(1, 3),
+                               stride=1,
+                               padding=(0, 1))
+        self.conv3 = nn.Conv2d(in_channels=64,
+                               out_channels=128,
+                               kernel_size=(1, 3),
+                               stride=1,
+                               padding=(0, 1))
+        self.conv4 = nn.Conv2d(in_channels=128,
+                               out_channels=256,
+                               kernel_size=(1, 3),
+                               stride=1,
+                               padding=0)
+        self.conv5 = nn.Conv2d(in_channels=256,
+                               out_channels=128,
+                               kernel_size=(1, 3),
+                               stride=1,
+                               padding=0)
+
+        self.intermediate_fc = nn.Linear(self.input_dim * (self.config['interval']-4) * 128, 64)
+
+        if self.config.num_classifiers == 1:
+            self.output_dim = 8
+            self.predictor = nn.Linear(64, self.output_dim)
+        else: 
+            self.output_dim = 4
+            self.predictors = nn.Sequential(OrderedDict([
+                ('delta_predictor', nn.Linear(64, self.output_dim)),
+                ('position_predictor', nn.Linear(64, self.output_dim))
+            ]))
+
+    def forward(self, conditions):
+        x = conditions.view(-1, 1, self.input_dim, self.config['interval']) # (batch_size, 1, 8, interval)
+        x = torch.relu(self.conv1(x)) # (batch_size, 32, 8, interval)
+        x = torch.relu(self.conv2(x)) # (batch_size, 64, 8, interval)
+        x = torch.relu(self.conv3(x)) # (batch_size, 128, 8, interval)
+        x = torch.relu(self.conv4(x)) # (batch_size, 256, 8, interval-2)
+        x = torch.relu(self.conv5(x)) # (batch_size, 128, 8, interval-4)
+        x = x.view(x.size(0), -1) # (batch_size, 8 * (interval-4) * 128)
+
+        # Intermaediate FC layer
+        intermediate_feature = torch.relu(self.intermediate_fc(x)) # (batch_size, 64)
+
+        if self.config.num_classifiers == 1:
+            delta_bbox = self.predictor(intermediate_feature) # (batch_size, output_dim)
+            return delta_bbox
+        
+        elif self.config.num_classifiers == 2:
+            delta_bbox = self.predictors.delta_predictor(intermediate_feature)
+            position = self.predictors.position_predictor(intermediate_feature)
+            return (delta_bbox, position)
--- train.py
+++ train.py
@@ -56,11 +56,19 @@ class Tracker(object):
                 pin_memory=True,
             )
 
-            print('\033[92m', end='') # green
-            self.step(train_set_loader, train=True)
-            print('\033[93m', end='') # yellow
-            self.step(val_set_loader, train=False, log_writer=False)
-            print('\033[0m', end='') # white
+            if self.config['network'] == 'huy_cnn':
+                print('\033[92m', end='') # green
+                self.huy_step(train_set_loader, train=True)
+                print('\033[93m', end='') # yellow
+                self.huy_step(val_set_loader, train=False, log_writer=False)
+                print('\033[0m', end='') # white
+
+            else:
+                print('\033[92m', end='') # green
+                self.step(train_set_loader, train=True)
+                print('\033[93m', end='') # yellow
+                self.step(val_set_loader, train=False, log_writer=False)
+                print('\033[0m', end='') # white
 
             if (epoch + 1) % self.config['eval_every'] == 0:
                 self.step(self.val_data_loader, train=False)
@@ -71,6 +79,84 @@ class Tracker(object):
 
             self.epoch += 1
 
+    def huy_step(self, data_loader, train=True, log_writer=True):
+        self.model.train() if train else self.model.eval()
+        epoch_loss = 0
+
+        delta_iou = 0
+        delta_ade = 0
+
+        position_iou = 0
+        position_ade = 0
+
+        for batch in tqdm(data_loader, desc=f"Epoch {self.epoch}/{self.config['epochs']}"):
+            for key in batch:
+                batch[key] = batch[key].to(self.device)
+
+            conditions = augment_data(batch['condition'].float())
+            delta_bbox = batch['delta_bbox'].float()
+            target_bbox = batch['cur_bbox'].float()
+
+            # Khúc này của Huy nè bà con
+            if self.config['num_classifiers'] == 1:
+                prediction = self.model(conditions)
+                predicted_position, predicted_delta_bbox = prediction[:, :4], prediction[:, 4:]
+                ground_truth = torch.cat([target_bbox, delta_bbox], dim=1)
+                loss = self.criterion(prediction, ground_truth)
+
+            elif self.config['num_classifiers'] > 1:
+                (predicted_delta_bbox, predicted_position) = self.model(conditions)
+                delta_loss = self.delta_criterion(predicted_delta_bbox, delta_bbox)
+                position_loss = self.position_criterion(predicted_position, target_bbox)
+                loss = delta_loss + position_loss
+
+            if train:
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()  
+
+            # MeanIoU and MeanADE
+            prev_bbox = conditions[:, -1, :4]
+            pred_bbox_from_delta = prev_bbox + predicted_delta_bbox # From delta_bbox
+            pred_bbox_from_position = predicted_position # From position
+            
+            w, h = batch['width'], batch['height']
+
+            orig_pred_from_delta = original_shape(pred_bbox_from_delta, w, h)
+            orig_pred_from_position = original_shape(pred_bbox_from_position, w, h)
+            orig_target = original_shape(target_bbox, w, h)
+
+            # Calculate IoU and ADE
+            delta_iou += calculate_iou(orig_pred_from_delta, orig_target)
+            delta_ade += calculate_ade(orig_pred_from_delta, orig_target)
+
+            position_iou += calculate_iou(orig_pred_from_position, orig_target)
+            position_ade += calculate_ade(orig_pred_from_position, orig_target)
+
+            epoch_loss += loss.item()
+
+        print(f"Epoch [{self.epoch}/{self.config['epochs']}],",
+              f"Loss: {epoch_loss / len(data_loader):.8f},",
+              f"MeanIoU_FromDelta: {delta_iou / len(data_loader):.8f},",
+              f"MeanADE_FromDelta: {delta_ade / len(data_loader):.8f}",
+              f"MeanIoU_FromPosition: {position_iou / len(data_loader):.8f},",
+              f"MeanADE_FromPosition: {position_ade / len(data_loader):.8f}")
+
+        if log_writer:
+            if train:
+                self.writer.add_scalar("Loss/train", epoch_loss / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanIoU_FromDelta/train", delta_iou / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanADE_FromDelta/train", delta_ade / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanIoU_FromPosition/train", position_iou / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanADE_FromPosition/train", position_ade / len(data_loader), self.epoch)
+            else:
+                self.writer.add_scalar("Loss/val", epoch_loss / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanIoU_FromDelta/val", delta_iou / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanADE_FromDelta/val", delta_ade / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanIoU_FromPosition/val", position_iou / len(data_loader), self.epoch)
+                self.writer.add_scalar("MeanADE_FromPosition/val", position_ade / len(data_loader), self.epoch)
+
+        self.scheduler.step()
 
     def step(self, data_loader, train=True, log_writer=True):
         self.model.train() if train else self.model.eval()
@@ -86,25 +172,24 @@ class Tracker(object):
             conditions = augment_data(batch['condition'].float())
             delta_bbox = batch['delta_bbox'].float()
 
-            with torch.amp.autocast('cuda'):
-                if self.config['network'] == 'autoencoder':
-                    recon, predicted_delta_bbox = self.model(conditions)
-                    recon = recon.view(-1, 9, 8)
-                    recon_loss = self.criterion(recon, conditions)
-                    delta_loss = self.criterion(predicted_delta_bbox, delta_bbox)
-                    loss = recon_loss + delta_loss
-                elif self.config['network'] == 'vae':
-                    recon, predicted_delta_bbox, mu, logvar = self.model(conditions)
-                    loss = self.model.loss_function(recon, conditions, mu, logvar)
-                else:
-                    predicted_delta_bbox = self.model(conditions)
-                    loss = self.criterion(predicted_delta_bbox, delta_bbox)
+            if self.config['network'] == 'autoencoder':
+                recon, predicted_delta_bbox = self.model(conditions)
+                recon = recon.view(-1, 9, 8)
+                recon_loss = self.criterion(recon, conditions)
+                delta_loss = self.criterion(predicted_delta_bbox, delta_bbox)
+                loss = recon_loss + delta_loss
+            elif self.config['network'] == 'vae':
+                recon, predicted_delta_bbox, mu, logvar = self.model(conditions)
+                loss = self.model.loss_function(recon, conditions, mu, logvar)
+
+            else:
+                predicted_delta_bbox = self.model(conditions)
+                loss = self.criterion(predicted_delta_bbox, delta_bbox)
 
             if train:
                 self.optimizer.zero_grad()
-                self.scaler.scale(loss).backward()
-                self.scaler.step(self.optimizer)
-                self.scaler.update()
+                loss.backward()
+                self.optimizer.step()
 
             # MeanIoU and MeanADE
             prev_bbox = conditions[:, -1, :4]
@@ -140,8 +225,8 @@ class Tracker(object):
 
 
     def _init_data_loader(self):
-        train_path = os.path.join(self.config['data_dir'], 'train')
-        val_path = os.path.join(self.config['data_dir'], 'val')
+        train_path = os.path.normpath(os.path.join(self.config['data_dir'], 'train'))
+        val_path = os.path.normpath(os.path.join(self.config['data_dir'], 'val'))
         train_dataset = TrackingDataset(train_path, config=self.config)
         val_dataset = TrackingDataset(val_path, config=self.config)
         print(f"Number of samples in the Train dataset: {len(train_dataset)}")
@@ -169,8 +254,10 @@ class Tracker(object):
             model = AutoEncoderPositionPredictor(self.config)
         elif self.config['network'] == 'cnn':
             model = Conv2dPredictor(self.config)
-        elif self.config['network'] == 'vae':
-            model = VAEPositionPredictor(self.config)
+        # elif self.config['network'] == 'vae':
+        #     model = VAEPositionPredictor(self.config)
+        elif self.config['network'] =='huy_cnn':
+            model = Huy_Conv2dPredictor(self.config)
 
         if self.config['resume']:
             if not os.path.exists(self.config['resume']):
@@ -186,10 +273,20 @@ class Tracker(object):
 
 
     def _init_optimizer(self):
-        self.criterion = nn.MSELoss()
+        if self.config['network'] == 'huy_cnn' and self.config['num_classifiers'] > 1:
+            if self.config['loss'] == 'l2':
+                self.delta_criterion = nn.MSELoss()
+                self.position_criterion = nn.MSELoss()
+            else:
+                self.delta_criterion = nn.SmoothL1Loss()
+                self.position_criterion = nn.SmoothL1Loss()
+        else:
+            if self.config['loss'] == 'l2':
+                self.criterion = nn.MSELoss()
+            else:
+                self.criterion = nn.SmoothL1Loss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.config['lr'])
         self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.98)
-        self.scaler = torch.amp.GradScaler(self.device)
 
     def _init_model_dir(self):
         if not self.config['model_dir'].startswith('experiments'):
